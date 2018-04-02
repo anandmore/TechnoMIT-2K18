88f2cb39645c84ffbe0b9bd1fe9ce8e39613d491
Anand More <anandmore1997.am@gmail.com>
2018-04-02 07:36:10 +0530

    Major code cleanup.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,59 +9,59 @@ if (subscribeImage) {  jQuery('#subscribe').css({ 'background-image':'url(' + su
 if (contactImage) {  jQuery('#contact').css({ 'background-image':'url(' + contactImage + ')' }); };
 
 jQuery(document).ready(function($) {
-  "use strict";
+"use strict";
 
-    var windowheight = jQuery(window).height();
-    if(windowheight > 650)
-    {
-         $('.pattern').removeClass('height-resize');
-    }
+var windowheight = jQuery(window).height();
+if(windowheight > 650)
+{
+$('.pattern').removeClass('height-resize');
+}
 
-  $('#main-menu #headernavigation').onePageNav({
-    currentClass: 'active',
-    changeHash: false,
-    scrollSpeed: 750,
-    scrollThreshold: 0.5,
-    scrollOffset: 0,
-    filter: '',
-    easing: 'swing'
-  });  
+$('#main-menu #headernavigation').onePageNav({
+currentClass: 'active',
+changeHash: false,
+scrollSpeed: 750,
+scrollThreshold: 0.5,
+scrollOffset: 0,
+filter: '',
+easing: 'swing'
+});  
 
-  $('.next-section .go-to-about').click(function() {
-      $('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
-    });
-    $('.next-section .go-to-events').click(function() {
-      $('html,body').animate({scrollTop:$('#events').offset().top}, 1000);
-    });
-    $('.next-section .go-to-sponsors').click(function() {
-      $('html,body').animate({scrollTop:$('#sponsors').offset().top}, 1000);
-    });
-    $('.next-section .go-to-contact').click(function() {
-      $('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
-    });
-    $('.next-section .go-to-page-top').click(function() {
-      $('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
-    });
+$('.next-section .go-to-about').click(function() {
+$('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
+});
+$('.next-section .go-to-events').click(function() {
+$('html,body').animate({scrollTop:$('#events').offset().top}, 1000);
+});
+$('.next-section .go-to-sponsors').click(function() {
+$('html,body').animate({scrollTop:$('#sponsors').offset().top}, 1000);
+});
+$('.next-section .go-to-contact').click(function() {
+$('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
+});
+$('.next-section .go-to-page-top').click(function() {
+$('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
+});
 
-    $(".news-letter").ajaxChimp({
-        callback: mailchimpResponse,
-        url: "http://jeweltheme.us10.list-manage.com/subscribe/post?u=a3e1b6603a9caac983abe3892&amp;id=257cf1a459" // Replace your mailchimp post url inside double quote "".  
-    });
+$(".news-letter").ajaxChimp({
+callback: mailchimpResponse,
+url: "http://jeweltheme.us10.list-manage.com/subscribe/post?u=a3e1b6603a9caac983abe3892&amp;id=257cf1a459" // Replace your mailchimp post url inside double quote "".  
+});
 
-    function mailchimpResponse(resp) {
-         if(resp.result === 'success') {
-         
-            $('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
-            
-        } else if(resp.result === 'error') {
-            $('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
-        }  
-    };
+function mailchimpResponse(resp) {
+if(resp.result === 'success') {
+
+$('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
+
+} else if(resp.result === 'error') {
+$('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
+}  
+};
 
 });
 
 $(window).load(function () {    
-    "use strict";
-    $("#loader").fadeOut();
-    $("#preloader").delay(350).fadeOut("slow");
+"use strict";
+$("#loader").fadeOut();
+$("#preloader").delay(350).fadeOut("slow");
 });
